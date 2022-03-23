aaea28a59d1582d6b605b08414c01ace2ad73445
Tovintsoa <tianatovintsoa@gmail.com>
2022-03-23 22:51:13 +0300

    1er commit

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Hello World</h1>');
 });
 
-server.listen(port,() => {
+server.listen(port,'0.0.0.0',() => {
     console.log(`Server running at port `+port);
 });
