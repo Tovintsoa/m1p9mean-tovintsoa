d3cc27426de29d84fe7f4d6916e4af1fd8ddfebe
Tovintsoa <tovintsoa1337@capri-nc.com>
2022-03-24 14:52:10 +0100

    Rectification

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+const root = path.join(__dirname, 'front');
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname, 'front/src/index.html'))
+});
+app.listen(port, () => {
+    console.log('server started');
+})
+/*const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hello World baby </h1>');
@@ -9,4 +21,8 @@ const server = http.createServer((req, res) => {
 
 server.listen(port,'0.0.0.0',() => {
     console.log(`Server running at port `+port);
-});
+});*/
+
+
+
+console.log('Listening on port '+ port);
