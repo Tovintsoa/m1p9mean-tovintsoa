1208de9b8264329c400c4ae7dfaa9bf56d7a30c2
Tovintsoa <tovintsoa1337@capri-nc.com>
2022-04-01 15:55:13 +0100

    Ok

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const userModel = require("../models/user");
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
+let mongoose = require('mongoose');
 
-app.get("/user", async (request, response) => {
-    const user = await userModel.find({});
-    console.log(user);
+app.get("/restaurant", async (request, response) => {
+    const user = await userModel.find({"role":"ROLE_RESTAURANT"});
+    response.json(user);
+});
+app.get("/plat/:userId", async function(req,res){
+
+    var o_id = mongoose.Types.ObjectId(req.params['userId']);
+    const list = await userModel.findOne({"_id":o_id});
+    res.json(list.plat);
 });
 app.post("/userAdd", async (request, response) => {
     const food = new userModel(request.body);
