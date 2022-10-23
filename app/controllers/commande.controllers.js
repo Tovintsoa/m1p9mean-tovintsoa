b1f3462cfd5e74b8aeb08262dd6696794980ce75
Tovintsoa <tianatovintsoa@gmail.com>
2022-10-23 22:27:35 +0300

    make it better

--- app/controllers/commande.controllers.js
+++ app/controllers/commande.controllers.js
@@ -4,9 +4,15 @@ let mongoose = require('mongoose');
 
 exports.getCommande = async (req,res) =>{
 
-    let commande = await commandeModel.find({
-        utilisateur:req.params['userId']
-    });
-    await res.json(commande);
+   if(req.params['userId'] !== 'undefined'){
+       let commande = await commandeModel.find({
+           utilisateur:req.params['userId']
+       });
+        console.log(commande);
+       await res.json(commande);
+   }
+   else{
+       await res.json([]);
+   }
 };
 
